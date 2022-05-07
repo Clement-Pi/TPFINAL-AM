2d9628ec6f9085ecc1a2e02a726be403c606efa5
Clement-Pi <clement.pilardeau@gmail.com>
2022-05-07 14:12:01 +0200

    interface main menu & main activity

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,20 +2,27 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.example.test.ui.main.MainFragment;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow();
-        }
+        setContentView(R.layout.activity_main);
+
+        final Button buttonback = findViewById(R.id.buttonback1);
+        buttonback.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    public void backToMainMenu(){
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/test/MainMenuActivity.java
@@ -0,0 +1,54 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.test.ui.main.MainFragment;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+
+        final Button button4 = findViewById(R.id.buttonquit);
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        final Button button1 = findViewById(R.id.buttonmain);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenuActivity.this, MainActivity.class));
+            }
+        });
+        /*
+        final Button button2 = findViewById(R.id.buttonquitj);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        final Button button3 = findViewById(R.id.buttonquitl);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });*/
+    }
+
+}
